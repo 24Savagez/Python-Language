4bf00ebdba0bdc99225028c7eaf579f29b0762f7
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-10-20 20:19:19 +0700

    11

---
+++ 11_input.py
@@ -0,0 +1,22 @@
+a = "Chutayu"
+b = "Adhitayawong"
+print(a,b)
+print(a+b)
+print("-----------------------------------")
+
+c = "10"
+d = 5
+e = int(c)+d
+print(e)
+print("-----------------------------------")
+
+#x = input()
+#print(type(x))
+#print("-----------------------------------")
+
+price = int(input("Input price : "))
+vat = 1.07
+result = price * vat
+print("Total",result)
+print("-----------------------------------")
+
