2ab862113967e2dba5ff2214f49ec83b0d11ce9d
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-10-20 19:49:41 +0700

    10

---
+++ 10_Operation.py
@@ -0,0 +1,45 @@
+x = 10
+y = 5
+z = x+y
+a = 10+5
+print(z)
+print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+CurrentMoney = 260
+incomePerDay = 400
+costPerDay = 150
+result = 260 + (400 * 30) - (150 * 30)
+resultz = CurrentMoney+(incomePerDay*30) - (costPerDay*30)
+print(result)
+print(resultz)
+print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+b = True
+c = False
+d = b and c
+print(d)
+print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+x = 10
+y = 5
+z = x > y
+print(z)
+print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+age = 19
+print("Enter your old :")
+print(age>18)
+print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+a = 10
+b = 12
+c = a + b
+a += 5
+print(a)
+print("++++++++++++++++++++++++++++++++++++++++++++++++++")
+
+money = 1000
+money = money - 50
+money -= 20
+print(money)
+print("++++++++++++++++++++++++++++++++++++++++++++++++++")
