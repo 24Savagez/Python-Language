a60c61d33103ff0feb91fdbaa56e2c7dd020bf5d
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-10-20 18:12:35 +0700

    05

---
+++ 05_DataType.py
@@ -0,0 +1,58 @@
+#data type
+print("-------------------")
+print(type("Hello World"))
+print("Hello world")
+
+print("-------------------")
+print(type(10))
+print(10)
+
+print("-------------------")
+print(type(10.1))
+print(10.1)
+
+print("-------------------")
+print(type(False))
+print(False)
+
+print("-------------------")
+print(type(1j))
+print(1j)
+
+print("-------------------")
+print(type(10+3.5))
+print(10+3.5)
+
+#integer Example
+print("-------------------")
+print(18/2)
+print(18+2)
+print(18-2)
+print(18*2)
+print(18%2)
+
+#float Example
+print("-------------------")
+print(3.14)
+print(3.14*2)
+print(3.14+10)
+print(3.14/2)
+print(3.14%2)
+print(10+3.0)
+
+#complex example
+print("-------------------")
+print(1j*1j)
+
+#boolean example
+print("-------------------")
+print(type(True))
+print(False)
+print(False and True)
+print(False or True)
+
+#String example
+print("-------------------")
+print("Hello Python")
+print("Hello"+"Python")
+print(("Hello "*3))
