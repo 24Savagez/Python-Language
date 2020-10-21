53053c993c294e8c5f06d7ba15aff93f5bb50644
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-10-21 18:30:22 +0700

    15

---
+++ 16_While.py
@@ -0,0 +1,13 @@
+usernameInput = ()
+passwordInput = ()
+
+while usernameInput != "admin" and passwordInput != "1234":
+
+    usernameInput = input("Username : ")
+    passwordInput = input("Password : ")
+
+    if usernameInput == "admin" and passwordInput == "1234":
+        print("Done !")
+    else:
+        print("Wrong,Try again...")
+        print()
