4fde3cc95f8cbb6ef4f73e9f0d496c8a6b095bd7
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-10-21 15:50:00 +0700

    12

--- 12_ifelse.py
+++ 12_ifelse.py
@@ -1,24 +1,24 @@
-try :
+try:
     score = int(input("Enter your Score : "))
-    if score <= 100 and score >= 0 :
-        if score >= 80 :
+    if score <= 100 and score >= 0:
+        if score >= 80:
             print("Grade : A")
-        elif score >= 75 :
+        elif score >= 75:
             print("Grade : B+")
-        elif score >= 70 :
+        elif score >= 70:
             print("Grad : B")
-        elif score >= 65 :
+        elif score >= 65:
             print("Grade : C+")
-        elif score >= 60 :
+        elif score >= 60:
             print("Grade : C")
-        elif score >= 55 :
+        elif score >= 55:
             print("Grade : D+")
-        elif score >= 50 :
+        elif score >= 50:
             print("Grade : D")
-        elif score >= 0 :
+        elif score >= 0:
             print("Gread : F")
-    else :
+    else:
         print("Invalid score")
-except :
+except:
     print("Error,please try 0 - 100")
 
