2ad6ad21dde01ca7dd8a71b86b94333371db4dc2
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-10-21 17:53:43 +0700

    13

---
+++ 13_NestedIf.py
@@ -0,0 +1,29 @@
+if True:
+    print("Hello Welcome !")
+    if True:
+        print("Yo! Ms.F")
+        if True:
+            print("WTF")
+print("------------------------------------------------")
+
+usernameInput = input("Username : ")
+passwordInput = input("Password : ")
+if usernameInput == "admin" and passwordInput == "1234":
+    print("Done !")
+
+    print("------iShop------")
+    print("1.Vat Calculator")
+    print("2.Price Calculator")
+    userSelected = int(input(": "))
+
+    if userSelected == 1:
+        price = int(input("Price(THB) : "))
+        vat = 0.07
+        result = price * vat
+        print("You must pay Vat :"+format(result,'.2f'))
+    elif userSelected == 2:
+        price = int(input("Price(THB) : "))
+        vat = 1.07
+        result = price * vat
+        print("You must pay Net :", result)
+print("------------------------------------------------")
