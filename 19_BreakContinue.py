c99f740a510581fcf0b19841ed7a6d3504154e78
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-10-21 21:11:49 +0700

    19

---
+++ 19_BreakContinue.py
@@ -0,0 +1,11 @@
+for x in range(11):
+    for y in range(12):
+        print(x+2,"x",y+1,"=",(x+2)*(y+1))
+    break
+print("----------------------------------------")
+
+for val in "hello world":
+    if val == "l":
+        continue
+    print(val)
+print("----------------------------------------")
