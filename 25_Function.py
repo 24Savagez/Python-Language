50202266c13a0c8ecc149a629b9e153f7993edb2
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-10-24 17:29:18 +0700

    25

---
+++ 25_Function.py
@@ -0,0 +1,25 @@
+def sayHey():
+    print("Hello World!")
+    myname()
+    print("GLHF")
+
+def myname():
+    print("Chutayu Adhitayawong")
+    print("GGWP")
+
+def addNumber(x,y):
+    return x+y
+
+x = int(input("Input number(x) : "))
+y = int(input("Input number(y) : "))
+
+sayHey()
+myname()
+
+z = addNumber(x,y)
+print("This value : %d"%(z))
+
+def GGWP():
+    pass
+
+
