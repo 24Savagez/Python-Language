ebde0930b9488e3ddbb77543aa7d9f95e3ebfeef
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-10-24 20:34:19 +0700

    28

---
+++ 28_String.py
@@ -0,0 +1,31 @@
+'''
+text = "Hello"
+print(text[0]+"Hello")
+print(text[1]*3)
+print(text[0:4])
+print("********************************")
+
+address = "18/215 Donmeuang Bangkok"
+print("Bangkok" in address)
+print("BKK" not in address)
+print("********************************")
+
+name = "FirstOnez"
+birthPlace = "Chaing Mai"
+print("Hello" + name , "and was born in" , birthPlace)
+print("********************************")
+
+print("Hello %s ! and was born in %s"%(name,birthPlace))
+
+name = input("Enter your name : ")
+lastname = input("Enter your lastname : ")
+print("Welcome %s %s To Maepaneekayhoom.inc"%(name.capitalize(),lastname.capitalize()))
+
+text = "First"
+textFormated = "Welcome %s"%(text)
+print(textFormated.center(21,"-"))
+print(len(text))
+'''
+
+
+
