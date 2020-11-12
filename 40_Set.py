b9a69bd499bbaa58e606603d8743a9644de1dbb2
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-11-12 21:43:55 +0700

    40

---
+++ 40_Set.py
@@ -0,0 +1,20 @@
+""""
+setTest = {"Apple","Banana","Orange"}
+
+for i in setTest:
+    if i == "Apple":
+        print(i)
+
+setTest.remove("Apple")
+print(setTest)
+"""
+fruits = {"apple","banana","orange"}
+print(fruits)
+fruits.remove("banana")
+print(fruits)
+fruits.add("Grape")
+print(fruits)
+
+
+#del delete all
+#del fruits
