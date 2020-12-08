c1b29cdc5119ce95a5c0bc28e6b85e3a28fdf6b2
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-12-08 10:36:34 +0700

    54

--- 54_Class.py
+++ 54_Class.py
@@ -1,26 +1,14 @@
 class Customer:
     name = ""
-    lastname = ""
+    lastName = ""
     age = 0
 
-    def addCart(self):
-        print("Added product to",self.name,self.lastname,"s cart")
+    def addCarl(self):
+        print("Added Product to",self.name,self.lastName+"'s carl")
 
 customer1 = Customer()
 customer1.name = "Chutayu"
-customer1.lastname = "Adhitayawong"
+customer1.lastName = "Adhitayawong"
 customer1.age = 20
-customer1.addCart()
-
-customer2 = Customer()
-customer2.name = "Asoka"
-customer2.lastname = "Yentramas"
-customer2.age = 21
-customer2.addCart()
-
-customer3 = Customer()
-customer3.name = "Rawiporn"
-customer3.lastname = "Kudsena"
-customer3.age = 49
-customer3.addCart()
+customer1.addCarl()
 
