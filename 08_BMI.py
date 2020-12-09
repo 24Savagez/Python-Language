8f4150676dc28756e609bf7dafe1fc68371e6b38
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-12-09 11:03:28 +0700

    08

--- 08_BMI.py
+++ 08_BMI.py
@@ -1,19 +1,19 @@
 x = float(input("Enter your Height(cm.) : "))
 y = float(input("Enter your Weight(kg.) : "))
 
-bmi = y/((x*x)/10000)
-bmi = round(bmi,1)
+bmi = y / ((x * x) / 10000)
+bmi = round(bmi, 1)
 
-print("Your BMI : ",bmi)
-#print('Your BMI : '+format(bmi,'.2f'))
+print("Your BMI : ", bmi)
+# print('Your BMI : '+format(bmi,'.2f'))
 
-if bmi > 30 :
+if bmi > 30:
     print("You are Obese")
-elif bmi > 25 :
+elif bmi > 25:
     print("You are Overweight")
-elif bmi > 18.5 :
+elif bmi > 18.5:
     print("You are Smart")
-elif bmi > 16 :
+elif bmi > 16:
     print("You are Thinness")
 else:
     print("You are Severe Thinness")
