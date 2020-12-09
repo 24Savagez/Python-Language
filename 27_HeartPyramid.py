42fd3d3c0684b2be9199fb6e25fa89e21b1ddce3
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-12-09 11:03:35 +0700

    27

--- 27_HeartPyramid.py
+++ 27_HeartPyramid.py
@@ -1,8 +1,8 @@
 # HeartPyramid
 for row in range(6):
     for col in range(7):
-        if(row == 0 and col%3 != 0) or (row == 1 and col%3 == 0) or (row - col == 2) or (row + col == 8):
-            print ("x",end="")
+        if(row == 0 and col % 3 != 0) or (row == 1 and col % 3 == 0) or (row - col == 2) or (row + col == 8):
+            print("x", end="")
         else:
-            print (end=" ")
+            print(end=" ")
     print()
