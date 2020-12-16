b9098f3129632491563b684d20f2960777cb1bb0
24Savagez <73163550+24Savagez@users.noreply.github.com>
2020-12-16 22:02:40 +0700

    OpenCv

--- 60_15_OpenCv_Thresholding02.py
+++ 60_15_OpenCv_Thresholding02.py
@@ -1,16 +1,33 @@
 import cv2
 import numpy as np
-
+import matplotlib.pyplot as plt
 
 image = cv2.imread('sudoku.png', 0)
 _, th1 = cv2.threshold(image, 127, 255, cv2.THRESH_BINARY)
-th2 = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 10)
-th3 = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 10)
+th2 = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+th3 = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 
-cv2.imshow("Image", image)
-#cv2.imshow("Binary", th1)
-cv2.imshow("Mean_c", th2)
-cv2.imshow("Gaussian_c", th3)
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+th1 = cv2.cvtColor(th1, cv2.COLOR_BGR2RGB)
+th2 = cv2.cvtColor(th2, cv2.COLOR_BGR2RGB)
+th3 = cv2.cvtColor(th3, cv2.COLOR_BGR2RGB)
 
+plt.subplot(1, 4, 1)
+plt.imshow(image)
+plt.title("Original")
+
+plt.subplot(1, 4, 2)
+plt.imshow(th1)
+plt.title("Binary")
+
+plt.subplot(1, 4, 3)
+plt.imshow(th2)
+plt.title("Mean_C")
+
+plt.subplot(1, 4, 4)
+plt.imshow(th3)
+plt.title("Gaussian_C")
+
+plt.show()
 cv2.waitKey(0)
 cv2.destroyAllWindows()
